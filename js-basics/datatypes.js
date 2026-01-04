1dbaaf948de50f098be66994bfc0d1ceb2995de3
shambhavi1406 <shambhavi200101@gmail.com>
2026-01-04 18:45:56 +0000

    Add variables and datatypes examples

---
+++ js-basics/datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3 ) we are usind nodejs,not browser
+
+let name = "Shambhavi"
+let age = 20
+let isstuding = false
+let state;
+// number - 2 to the power of 53
+// bigInt
+// boolean - true/false
+// string - ""
+// null - standalone value
+// undefined - not assigned a value yet
+// symbol - unique
+
+// object
+console.log(typeof false);
+console.log(typeof "Shambhavi");
+console.log(typeof 20);
+ let score = "33abc"
+ console.log(typeof score)
+ let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 - true, 0 - false
+//"" - false
+//"hello" - true
+
+let someNumber = 56
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ js-basics/variables.js
@@ -0,0 +1,14 @@
+
+const accountId = 123435
+let accountEmail = "sham234@gmail.com"
+ var accountPassword = "12345" 
+//  prefer not to use var
+accountCity = "Jaipur"
+
+// accountId = 67 // not allowed
+
+accountEmail = "gh@abc.com"
+accountPassword = "67gh"
+accountCity = "pune"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
